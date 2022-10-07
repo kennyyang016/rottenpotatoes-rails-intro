9fa0e88f6ba28502c3b0cc266c003db790fd48e4
Kenny Yang <kennyyang016@berkeley.edu>
2022-10-07 19:30:56 +0000

    part 3 complete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,41 +1,79 @@
 class MoviesController < ApplicationController
 
     def show
+      session[:active] = true
       id = params[:id] # retrieve movie ID from URI route
       @movie = Movie.find(id) # look up movie by unique ID
       # will render app/views/movies/show.<extension> by default
     end
-  
+
     def index
       # byebug
       @all_ratings = Movie.all_ratings
       @selected = params[:order]
-      if params[:ratings].nil?
-        @movies = Movie.with_order(Movie.with_ratings(nil), 
+      if params[:ratings].nil? && params[:order].nil?
+        if session[:active]
+          if session[:ratings]
+            @movies = Movie.with_order(Movie.with_ratings(
+              session[:ratings].keys), session[:order])
+            @ratings_to_show = session[:ratings]
+            @selected = session[:order]
+          else
+            @movies = Movie.with_order(Movie.with_ratings(
+              nil), session[:order])
+            @ratings_to_show = @all_ratings
+            @selected = session[:order]
+          end
+        else
+          @movies = Movie.with_order(Movie.with_ratings(nil), nil)
+          @ratings_to_show = @all_ratings
+          session[:rating] = nil
+          session[:order] = nil
+        end
+        session[:active] = false
+        # @movies = Movie.with_order(Movie.with_ratings(nil), nil)
+        # @ratings_to_show = @all_ratings
+        # session[:ratings] = nil
+        # session[:order] = nil
+      elsif params[:ratings].nil?
+        @movies = Movie.with_order(Movie.with_ratings(nil),
           params[:order])
         @ratings_to_show = @all_ratings
+        session[:ratings] = @ratings_to_show
+        session[:order] = params[:order]
       else
-        @movies = Movie.with_order(Movie.with_ratings(
-          params[:ratings].keys), params[:order])
+        if params[:ratings].kind_of?(Array)
+          @movies = Movie.with_order(Movie.with_ratings(
+            params[:ratings]), params[:order])
+        else
+          @movies = Movie.with_order(Movie.with_ratings(
+            params[:ratings].keys), params[:order])
+        end
         @ratings_to_show = params[:ratings]
+        session[:ratings] = @ratings_to_show
+        session[:order] = params[:order]
       end
     end
   
     def new
       # default: render 'new' template
+      session[:active] = true
     end
   
     def create
+      session[:active] = true
       @movie = Movie.create!(movie_params)
       flash[:notice] = "#{@movie.title} was successfully created."
       redirect_to movies_path
     end
   
     def edit
+      session[:active] = true
       @movie = Movie.find params[:id]
     end
   
     def update
+      session[:active] = true
       @movie = Movie.find params[:id]
       @movie.update_attributes!(movie_params)
       flash[:notice] = "#{@movie.title} was successfully updated."
@@ -43,6 +81,7 @@ class MoviesController < ApplicationController
     end
   
     def destroy
+      session[:active] = 1
       @movie = Movie.find(params[:id])
       @movie.destroy
       flash[:notice] = "Movie '#{@movie.title}' deleted."
